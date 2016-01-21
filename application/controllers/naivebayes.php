49e9415d6d23a92a02b22937735c4415e4236ada
k-kuwahara <zensin0082@gmail.com>
2016-01-22 01:07:22 +0900

    クラス名の修正

--- application/controllers/naivebayes.php
+++ application/controllers/naivebayes.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Naivebayes extends My_Controller {
+class Naivebayes extends MY_Controller {
    function __construct() {
       parent::__construct();
       $this->wordCount = [];
